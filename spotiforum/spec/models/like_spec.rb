6d66c30635f951982df8761b0ad01c4f0f7f6ca2
Ale-ssio <vernarelli.1946128@studenti.uniroma1.it>
2023-07-16 12:02:32 +0200

    Unit tests del primo test verticale conclusi, manca terminare i Function tests

---
+++ spotiforum/spec/models/like_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe Like do
+	# Creo un utente e un post perché ne ho bisogno per poter creare un like.
+	# Voglio verificare che il like sia creato con utente e post corretti.
+	let(:user) { User.create!(name: 'UserTest', email: 'ut@mail.com', photo: 1, spotify: false, song: nil, google: false, encrypted_password: '123456') }
+	let(:post) { Post.create!(content: 'Il mio post di test', user_id: user.id, tag: '#test') }
+	let(:like) { Like.create!(user_id: user.id, post_id: post.id) }
+	
+	it "has a user" do
+		# Verifico che il like sia stato correttamente creato con l'esatto user_id che ho passato.
+		expect(like.user_id).to eq(user.id)
+	end
+	it "has a post" do
+		# Verifico che il like sia stato correttamente creato con l'esatto post_id che ho passato.
+		expect(like.post_id).to eq(post.id)
+	end
+	
+end
