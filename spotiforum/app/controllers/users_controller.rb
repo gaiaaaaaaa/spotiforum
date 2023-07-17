54e266a96e8be71a141804325801581f8c641d74
Ale-ssio <vernarelli.1946128@studenti.uniroma1.it>
2023-07-17 15:35:14 +0200

    Si possono cercare i post tramite tag, i tag devono avere uno specifico formato

--- spotiforum/app/controllers/users_controller.rb
+++ spotiforum/app/controllers/users_controller.rb
@@ -171,6 +171,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :photo, :spotify, :song, :google, :password, :mostlistenedsong, :mostlistenedartist)
+      params.require(:user).permit(:name, :email, :photo, :image, :spotify, :song, :google, :password, :mostlistenedsong, :mostlistenedartist)
     end
 end
